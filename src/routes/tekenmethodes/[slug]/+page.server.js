f8e05b4d434c6125ab2f606a10a9bc809b328725
Daniquedejong <danique.de.jong3@hva.nl>
2024-04-23 10:11:54 +0200

    🖊️Semantic name for GraphQl query
    I changed the name of this query.

--- src/routes/tekenmethodes/[slug]/+page.server.js
+++ src/routes/tekenmethodes/[slug]/+page.server.js
@@ -1,11 +1,11 @@
 import { gql } from "graphql-request";
 import { hygraph } from "$lib/utils/hygraph.js";
 
-import getQueryUrl from "$lib/queries/url.js";
+import methodsBeschrijvingQueryUrl from "$lib/queries/url.js";
 
 export const load = async ({ params }) => {
 	const { slug } = params;
-	const queryUrl = getQueryUrl(gql, slug);
+	const queryUrl = methodsBeschrijvingQueryUrl(gql, slug);
 
     return await hygraph.request(queryUrl);
 }
