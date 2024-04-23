8837dad59f042ed61748ebdf2442d80f8ce7c553
Daniquedejong <danique.de.jong3@hva.nl>
2024-04-23 10:27:21 +0200

    🖊️Semantic Url names
    The URL files now have semantic names.
    - methods.Url.js
    - articlesUrl.js

--- src/routes/tekenmethodes/[slug]/+page.server.js
+++ src/routes/tekenmethodes/[slug]/+page.server.js
@@ -1,7 +1,7 @@
 import { gql } from "graphql-request";
 import { hygraph } from "$lib/utils/hygraph.js";
 
-import methodsBeschrijvingQueryUrl from "$lib/queries/url.js";
+import methodsBeschrijvingQueryUrl from "$lib/queries/methodsUrl.js";
 
 export const load = async ({ params }) => {
 	const { slug } = params;
